33ead325d8dc23c6ddbe8d66ded49ac88a48302a
Jose Mendieta <mendietacruzjose@gmail.com>
2026-06-22 14:19:06 -0600

    Se genera reader de properties y también archivo config properties

--- src/test/java/utils/PropertiesReader.java
+++ src/test/java/utils/PropertiesReader.java
@@ -1,4 +1,27 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertiesReader {
+    private static Properties properties;
+
+    static {
+        try {
+            properties = new Properties();
+
+            FileInputStream fis =
+                    new FileInputStream("src/test/resources/config.properties");
+
+            properties.load(fis);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error al cargar config.properties");
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
 }
