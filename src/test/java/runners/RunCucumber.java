a04c7616a21b583415c2d8e474873bf9e54377b2
Jose Mendieta <mendietacruzjose@gmail.com>
2026-06-22 17:47:35 -0600

    Se agregó run cucumber y dependencias de junit y cucumber

--- src/test/java/runners/RunCucumber.java
+++ src/test/java/runners/RunCucumber.java
@@ -1,4 +1,26 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/api",
+        glue = {
+                "steps",
+                "hooks"
+        },
+        plugin = {
+                "pretty",
+                "html:target/reports/cucumber.html",
+                "json:target/reports/cucumber.json"
+        },
+        monochrome = true,
+        dryRun = false,
+        publish = false
+)
+
 public class RunCucumber {
+
 }
