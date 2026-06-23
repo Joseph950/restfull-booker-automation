5423467d5a2806efe6dc87b9914d63ae6c2a09fd
Jose Mendieta <mendietacruzjose@gmail.com>
2026-06-22 23:37:01 -0600

    Se corrigieron los detalles del reporte de cucumber

--- src/test/java/runners/RunCucumber.java
+++ src/test/java/runners/RunCucumber.java
@@ -3,7 +3,9 @@ package runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import reports.CucumberReport;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -16,11 +18,21 @@ import org.junit.runner.RunWith;
                 "pretty",
                 "html:target/reports/cucumber.html",
                 "json:target/reports/cucumber.json"
+                //"html:target/advanced-report/cucumber-html-reports/cucumber.html",
+                //"json:target/advanced-report/cucumber-html-reports/cucumber.json"
         },
         publish = true,
         tags = "@first"
 )
 
 public class RunCucumber {
+        public RunCucumber() {
+        }
+
+        @AfterClass
+        public static void tearDown() {
+                CucumberReport.generateReport();
+        }
 
 }
+//target\reports\cucumber.json (El sistema no puede encontrar la ruta especificada)
